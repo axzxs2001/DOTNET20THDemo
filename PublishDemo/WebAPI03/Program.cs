c5af189e39f256c39259c2fb0ec5d1043769b2b0
素伟 桂 <axzxs2001@163.com>
2022-04-08 18:23:54 +0900

    重构

--- PublishDemo/WebAPI03/Program.cs
+++ PublishDemo/WebAPI03/Program.cs
@@ -1,15 +1,44 @@
+//var builder = WebApplication.CreateBuilder();
 
+//var app = builder.Build();
+//app.Urls.Add("http://*:5003");
 
-var builder = WebApplication.CreateBuilder();
+//app.MapGet("/test", (ILogger<Program> logger) =>
+//{
+//    logger.LogInformation("请求WebAPI03的test");
+//    return DateTime.Now;
+//});
+
+//app.Run();
+
+var builder = WebApplication.CreateBuilder(args);
+
 
 var app = builder.Build();
 app.Urls.Add("http://*:5003");
 
-app.MapGet("/test", (ILogger<Program> logger) =>
+
+var summaries = new[]
 {
-    logger.LogInformation("请求WebAPI03的test");
-    return "WebAPI03 OK";
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/test", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return System.Text.Json.JsonSerializer.Serialize(forecast);
 });
 
 app.Run();
 
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
