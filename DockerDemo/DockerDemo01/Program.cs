42287e49c34bac2dba3877267bbb2900d40e983c
桂素伟 <axzxs2001@163.com>
2022-04-19 21:41:17 +0900

    优化代码

--- DockerDemo/DockerDemo01/Program.cs
+++ DockerDemo/DockerDemo01/Program.cs
@@ -3,8 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.MapGet("/test", () =>
+{
+    return $"OK:{DateTime.Now}";
+});
 
-app.MapGet("/getfile", () =>
+app.MapGet("/getfiles", () =>
 {
     var files = System.IO.Directory.GetFiles("/app/data");
     return files;
